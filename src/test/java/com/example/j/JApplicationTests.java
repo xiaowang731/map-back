b246a55a73a450ae4fb63e736209697204918f9f
Adss <1726612629@qq.com>
2024-12-24 09:40:02 +0800

    项目初始化

---
+++ src/test/java/com/example/j/JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
